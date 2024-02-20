4621fc7d69bf7e15bb99291b983718faab74cf15
Megh Patel <megh@Meghs-MacBook-Air.local>
2024-02-19 19:42:20 -0600

    some changes to swordsman.cpp

--- Characters/swordsman.cpp
+++ Characters/swordsman.cpp
@@ -3,8 +3,9 @@
 class SwordsMan : public CharacterSuper{
 public:
     SwordsMan(){
-        speed = speed/2;
-        strength = strength/2;
+        armor = 100;
+        speed = 50;
+        strength = 50;
         addToInventory("Heaven Slasher");
         addToInventory("Manual");
         addSpecialAbility("Killer Move");
