e1bb19e7cad44f441d7a4f45a0dcd876151da8b2
Sam Mercier <mercierse@slu.edu>
2024-02-21 18:04:12 -0600

    story narrative with half built context switcher and character selection

--- Characters/characterSuper.cpp
+++ Characters/characterSuper.cpp
@@ -52,5 +52,3 @@ public:
         health+=recovery;
     }
 };
-
-
--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-#include "Characters/characterSuper.cpp"
-#include "Characters/Ranger.cpp"
-#include "narrativeSuper.cpp"
+#include "narrativeStory.cpp"
+#include "Ranger.cpp"
+#include "Guardian.cpp"
+#include "Swordsman.cpp"
+#include "Wizard.cpp"
+#include "characterSuper.cpp"
+
 using namespace std;
 
 int main(){
-    Ranger ranger;
-    cout << ranger.getSpeed() << endl;
+    int player;
+    cout<<"Welcome to the world of Warbles"<<endl<<"Choose a character for your adventure"<<endl<<"Option 1 Guardian "<<endl<<
+    "Option 2 Ranger"<<endl<<"Option 3 Swordsman"<<endl<<"Option 4 Wizard"<<endl;
+    cin >> player ;
+    NarrativeStory mainStory(player);
+    
+    mainStory.moveThroughStory();
+    mainStory.moveThroughStory();
     return 0;
-}
+};
