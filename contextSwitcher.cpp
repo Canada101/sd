0f313d1c73c3773289cc2757068d82aa2c18311a
Megh Patel <megh@Meghs-MacBook-Air.local>
2024-02-25 22:47:47 -0600

    adding the main story which will run if you select only option 1

--- contextSwitcher.cpp
+++ contextSwitcher.cpp
@@ -6,9 +6,21 @@ class ContextSwitcher
     public:
     //1 = main story 2 = npc dialogue 3 = combat
     int context;
+    int input;
     ContextSwitcher()
     {
-
+        input = 1;
+        NarrativeSuper story;
+        int player = story.startMenu();
+    
+        NarrativeStory mainStory(player);
+        while(1){
+            mainStory.moveThroughStory();
+            cin >> input;
+            if(input != 1){
+                break;
+            }
+        }
     }; 
     int changeContext(int contextType)
     {
--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include "narrativeStory.cpp"
+#include "contextSwitcher.cpp"
 #include "Characters/Ranger.cpp"
 
 using namespace std;
 
 int main(){
-    NarrativeSuper story;
-    int player = story.startMenu();
-    
-    NarrativeStory mainStory(player);
-    
-    mainStory.moveThroughStory();
-    return 0;
+    ContextSwitcher switcher;
 };
