0b76e7bd4fe9735cfdef0179f61450342e31519c
David Ferry <dferry@slu.edu>
2024-04-29 14:33:25 -0500

    flexible failure mode

--- runTest.cpp
+++ runTest.cpp
@@ -4,14 +4,16 @@
 
 int main(){
 
+	bool returnValue = 0;
 	UnitTests ut ("unittest_output");
 
 	bool result = ut.testHealth();
 	if( result == false ){
 		std::cerr << "testHealth test FAILED" << std::endl;
+		returnValue = -1;
 	} else {
 		std::cout << "testHealth passed" << std::endl;
 	}
 
-	return -1;
+	return returnValue;
 }
